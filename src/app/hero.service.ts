7bc4353116c74b66fb2700f7bda4fea4fe910fcf
muhseenat <mucktd777@gmail.com>
2022-06-02 16:53:09 +0530

    tap and catch error added for fetchinf single hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -19,14 +19,17 @@ private heroesUrl = 'api/heroes';
       this.messageService.add(`HeroService: ${message}`);
     }
   getHeroes():Observable<Hero []>{
-return this.http.get<Hero[]>(this.heroesUrl).pipe(catchError(this.handleError<Hero[]>('getHeroes',[])))
+return this.http.get<Hero[]>(this.heroesUrl).pipe(tap(_=>this.log('fetched heroes')),
+catchError(this.handleError<Hero[]>('getHeroes',[])))
     
   }
 
 getHero(id:number):Observable<Hero>{
-  const hero=HEROES.find(h=>h.id===id)!;
-  this.messageService.add(`HeroService: fetched hero id=${id}`);
-  return of(hero);
+const url=`${this.heroesUrl}/${id}`;
+return this.http.get<Hero>(url).pipe(
+  tap(_=>this.log(`fetched hero id =${id}`)),
+  catchError(this.handleError<Hero>(`getHeroid=${id}`))
+)
 }
 
 //ERROR HANDLING
