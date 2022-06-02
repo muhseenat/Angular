d9abebcf23dc4e9873862fbc9667866c17db9a3f
muhseenat <mucktd777@gmail.com>
2022-06-02 11:08:38 +0530

    Error fixed and started message component and service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Hero } from 'src/hero';
 import { HEROES } from 'src/mock-heroes';
+import { Observable,of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private heroService:HeroService) { }
+  constructor() { }
 
-  getHeroes():Hero []{
-    return HEROES;
+  getHeroes():Observable<Hero []>{
+    const heroes=of(HEROES)
+    return heroes
   }
 
 
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from 'src/hero';
-
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -8,7 +8,7 @@ import { Hero } from 'src/hero';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private heroService:HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -21,6 +21,7 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.hero = this.heroService.getHeroes();
+  this.heroService.getHeroes().subscribe(hereos=>this.hero=hereos)
+    
   }
 }
