1f50abc0cd28cede792e54f073be4453495a43a7
muhseenat <mucktd777@gmail.com>
2022-06-02 12:22:15 +0530

    Messages added when  select heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,23 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from 'src/hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  
+  hero:Hero[]=[]
+  selectedHero?:Hero;
 
-  constructor(private heroService:HeroService) { }
+  constructor(private heroService:HeroService,
+    private messageService:MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
-  hero:Hero[]=[]
-  selectedHero?:Hero;
   onSelect(hero:Hero):void{
     this.selectedHero=hero;
+    this.messageService.add(`HerosCompponent:Selected hero id=${hero.id}`)
   }
 
   getHeroes(): void {
